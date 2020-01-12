9fcd5c6a573394d08345c1faccc545d02d87e727
Miguel Duque Filho <miguel.duque@globo.com>
2020-01-12 17:58:58 -0300

    implementado middlewares e consulta users

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,4 @@
 module.exports = app => {
+  app.use("*", app.src.api.middlewares.timescan);
   app.route("/users").get(app.src.api.user.get);
 };
-
-// const routes = require("express").Router();
-
-// const authMiddleware = require("./app/middleware/auth");
-
-// const SessionController = require("./app/controllers/SessionControler");
-
-// routes.post("/sessions", SessionController.store);
-
-// routes.use(authMiddleware);
-
-// routes.get("/dashboard", (req, res) => {
-//   return res.status(200).send("dashboard");
-// });
-
-// module.exports = routes;
