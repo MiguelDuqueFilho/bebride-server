4d6859441f16341a3c74d4fc54e54a29fc927d8e
Miguel Duque Filho <miguel.duque@globo.com>
2020-01-12 19:49:34 -0300

    acerto gitignore para os teste do jest

---
+++ __tests__/integration/users.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+const app = require("../../src/app");
+
+describe("Test jest", () => {
+  it("should sum two numbers", () => {
+    const x = 2;
+    const y = 4;
+    const sum = x + y;
+    expect(sum).toBe(6);
+  });
+});
+describe("Users consult", () => {
+  it("List users from database", async () => {
+    const response = await request(app)
+      .get("/users")
+      .send();
+    expect(response.status).toBe(200);
+  });
+});
+// describe("Users register", () => {
+//   it("Insert user in database", () => {});
+// });
