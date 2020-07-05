e251a765e676b61308ccc7ad82e4d516dce1e965
Miguel Duque Filho <miguel.duque@globo.com>
2020-07-05 15:31:31 -0300

    acerto sequelize

--- src/database/seeders/20200403000002-carga-inicial-task-status.js
+++ src/database/seeders/20200403000002-carga-inicial-task-status.js
@@ -6,31 +6,31 @@ module.exports = {
       {
         id: 1,
         task_status_name: "Inicial",
-        task_status_color: "info"
+        task_status_color: "info",
       },
       {
         id: 2,
         task_status_name: "Em dia",
-        task_status_color: "success"
+        task_status_color: "success",
       },
       {
         id: 3,
         task_status_name: "Alerta",
-        task_status_color: "warning"
+        task_status_color: "warning",
       },
       {
         id: 4,
         task_status_name: "Atrasada",
-        task_status_color: "danger"
-      }
+        task_status_color: "danger",
+      },
       {
         id: 5,
         task_status_name: "Concluída",
-        task_status_color: "secundary"
-      }
+        task_status_color: "secundary",
+      },
     ]);
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("task_status", null, {});
-  }
+  },
 };
