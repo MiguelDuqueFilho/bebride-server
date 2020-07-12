4e1020b7743cbfce7ac3d527b7c8e60fb62d8eb3
Miguel Duque Filho <miguel.duque@globo.com>
2020-07-12 14:46:45 -0300

    delete seeds para a producao'

--- src/database/seeders/20200116015009-carga-inicial-users.js
+++ src/database/seeders/20200116015009-carga-inicial-users.js
@@ -8,33 +8,12 @@ module.exports = {
         user_email: "admin@globo.com",
         password_hash:
           "$2a$08$prJzB5C70pZxT/jPqAisfus70ecaRcGS5visJkuqRnHn5oBxJklQS",
-        user_type: 1
+        user_type: 1,
       },
-      {
-        user_name: "Miguel Duque Filho",
-        user_email: "miguel.duque@globo.com",
-        password_hash:
-          "$2a$08$prJzB5C70pZxT/jPqAisfus70ecaRcGS5visJkuqRnHn5oBxJklQS",
-        user_type: 2
-      },
-      {
-        user_name: "Wilma Area Duque",
-        user_email: "wilma.duque@globo.com",
-        password_hash:
-          "$2a$08$LoCjPaMb2nJvSHlxougvbOjuRONf4zIbKv4oG2HXdi.lyNk5Vq5RO",
-        user_type: 3
-      },
-      {
-        user_name: "Cliente ",
-        user_email: "cliente@globo.com",
-        password_hash:
-          "$2a$08$LoCjPaMb2nJvSHlxougvbOjuRONf4zIbKv4oG2HXdi.lyNk5Vq5RO",
-        user_type: 0
-      }
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("users", null, {});
-  }
+  },
 };
